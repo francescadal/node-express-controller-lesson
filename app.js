58bf7c944ff58489bcfd3d49fc3e649b7a250a63
Saad Rhoulam <saad@rhoulam.com>
2015-11-17 16:32:22 -0500

    Remove all references to view engine and serve-static.

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,7 +18,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
