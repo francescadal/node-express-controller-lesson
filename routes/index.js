ea14bb45083a7d572ec6c946c2ce2c3fe7d46cb1
Saad Rhoulam <saad@rhoulam.com>
2015-11-19 15:22:11 -0500

    Recrapify routes for demonstration purposes.

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,23 @@ var exCtrl = require('../controllers/examplePOJOController');
 
 /* GET home page. */
 router.route('/').
-    get(exCtrl.get).
-    post(exCtrl.post).
-    patch(exCtrl.patch).
-    delete(exCtrl.delete);
+    get(function exampleGet(req, res) {
+        res.status(200).json({
+            example : "Example!"
+        });
+    }).
+    post(function examplePost(req, res) {
+        if(req.body) {
+            // echo
+            res.status(200).json(req.body);
+        } else {
+            // nothing to echo; incorrect request
+            res.sendStatus(400);
+        }
+    }).
+    all(function examplePatch(req, res) {
+        // not allowed!
+        res.sendStatus(405);
+    });
 
 module.exports = router;
